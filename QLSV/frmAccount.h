45933f144f9bc48f00e06d4d454b8946d01f028d
lUwUth <131012608+lUwUth@users.noreply.github.com>
2023-04-26 23:12:25 +0700

    Add files via upload

---
+++ QLSV/frmAccount.h
@@ -0,0 +1,270 @@
+#pragma once
+
+namespace QLSV {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for frmManageScore
+	/// </summary>
+	public ref class frmAccount : public System::Windows::Forms::Form
+	{
+	public:
+		frmAccount(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~frmAccount()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	protected:
+	private: System::Windows::Forms::Panel^ panel8;
+
+
+
+
+
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label4;
+
+
+
+
+
+
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Label^ label8;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->panel8 = (gcnew System::Windows::Forms::Panel());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->panel8->SuspendLayout();
+			this->panel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// panel8
+			// 
+			this->panel8->BackColor = System::Drawing::Color::Cornsilk;
+			this->panel8->Controls->Add(this->dataGridView1);
+			this->panel8->Controls->Add(this->button4);
+			this->panel8->Controls->Add(this->button3);
+			this->panel8->Controls->Add(this->button2);
+			this->panel8->Controls->Add(this->textBox3);
+			this->panel8->Controls->Add(this->label3);
+			this->panel8->Controls->Add(this->textBox2);
+			this->panel8->Controls->Add(this->label4);
+			this->panel8->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->panel8->Location = System::Drawing::Point(0, 50);
+			this->panel8->Name = L"panel8";
+			this->panel8->Size = System::Drawing::Size(764, 461);
+			this->panel8->TabIndex = 29;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(316, 239);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(174, 20);
+			this->textBox3->TabIndex = 28;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Century Gothic", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(222, 240);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(75, 16);
+			this->label3->TabIndex = 27;
+			this->label3->Text = L"Username:";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(316, 283);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(174, 20);
+			this->textBox2->TabIndex = 26;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Century Gothic", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(226, 284);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(71, 16);
+			this->label4->TabIndex = 25;
+			this->label4->Text = L"Password:";
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::Color::BurlyWood;
+			this->panel1->Controls->Add(this->label8);
+			this->panel1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panel1->Location = System::Drawing::Point(0, 0);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(764, 50);
+			this->panel1->TabIndex = 27;
+			// 
+			// label8
+			// 
+			this->label8->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->label8->AutoSize = true;
+			this->label8->Font = (gcnew System::Drawing::Font(L"Century Gothic", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label8->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->label8->Location = System::Drawing::Point(311, 9);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(110, 28);
+			this->label8->TabIndex = 0;
+			this->label8->Text = L"Account";
+			this->label8->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// button4
+			// 
+			this->button4->BackColor = System::Drawing::Color::DarkKhaki;
+			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button4->Font = (gcnew System::Drawing::Font(L"Century Gothic", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button4->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->button4->Location = System::Drawing::Point(220, 330);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(90, 26);
+			this->button4->TabIndex = 67;
+			this->button4->Text = L"Add";
+			this->button4->UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::Coral;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Century Gothic", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button3->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->button3->Location = System::Drawing::Point(412, 330);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(90, 26);
+			this->button3->TabIndex = 66;
+			this->button3->Text = L"Delete";
+			this->button3->UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::Khaki;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Century Gothic", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)));
+			this->button2->Location = System::Drawing::Point(316, 330);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(90, 26);
+			this->button2->TabIndex = 65;
+			this->button2->Text = L"Update";
+			this->button2->UseVisualStyleBackColor = false;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->AllowUserToOrderColumns = true;
+			this->dataGridView1->BackgroundColor = System::Drawing::Color::Wheat;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->dataGridView1->GridColor = System::Drawing::Color::Beige;
+			this->dataGridView1->Location = System::Drawing::Point(0, 0);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(764, 201);
+			this->dataGridView1->TabIndex = 68;
+			// 
+			// frmAccount
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::LemonChiffon;
+			this->ClientSize = System::Drawing::Size(764, 511);
+			this->Controls->Add(this->panel8);
+			this->Controls->Add(this->panel1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Name = L"frmAccount";
+			this->Text = L"frmAccount";
+			this->panel8->ResumeLayout(false);
+			this->panel8->PerformLayout();
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
